01464e525e9d6234e9bf05f58a048f2b486242df
Bowen <bowenxu.09.10@gmail.com>
2025-11-10 20:27:29 +0800

    Optimize the method AddLast

--- lab3/timingtest/AList.java
+++ lab3/timingtest/AList.java
@@ -33,8 +33,9 @@ public class AList<Item> {
 
     /** Inserts X into the back of the list. */
     public void addLast(Item x) {
+        int factor = 2;
         if (size == items.length) {
-            resize(size + 1);
+            resize(size * factor);
         }
 
         items[size] = x;
