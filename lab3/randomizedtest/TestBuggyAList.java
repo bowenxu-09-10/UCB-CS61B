db59b6a00cf3b12e7625364b20ba12471e1f5aa8
Bowen <bowenxu.09.10@gmail.com>
2025-11-10 21:41:39 +0800

    Add JUnit Test

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,50 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> buggy = new BuggyAList<>();
+        for (int i = 4; i < 7; i++) {
+            correct.addLast(i);
+            buggy.addLast(i);
+        }
+        for (int i = 0; i < 2; i++) {
+            correct.removeLast();
+            buggy.removeLast();
+        }
+        int expected = correct.removeLast();
+        int actual = buggy.removeLast();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> lst = new BuggyAList<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 3);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                lst.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int size = L.size();
+                System.out.println("size: " + size);
+                if (L.size() > 0) {
+                    int exp = L.getLast();
+                    int act = lst.getLast();
+                    assertEquals(exp, act);
+                    int expected = L.removeLast();
+                    int actual = lst.removeLast();
+                    assertEquals(expected, actual);
+                }
+            }
+        }
+    }
 }
