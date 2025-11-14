ed3a4e19574965f2be44887d365373e1bc11a8ed
Bowen <bowenxu.09.10@gmail.com>
2025-11-14 22:50:21 +0800

    Delete redundent marks

--- proj1/deque/Deque.java
+++ proj1/deque/Deque.java
@@ -8,17 +8,17 @@ public interface Deque<T> {
     /**
      * Adds an item of type T to the front of the deque.
      */
-    public void addFirst(T item);
+    void addFirst(T item);
 
     /**
      * Adds an item of type T to the back of the deque.
      */
-    public void addLast(T item);
+    void addLast(T item);
 
     /**
      * Return true if deque is empty.
      */
-    default public boolean isEmpty() {
+    default boolean isEmpty() {
         if (this.size() == 0) {
             return true;
         }
@@ -28,31 +28,31 @@ public interface Deque<T> {
     /**
      * Returns the number of items in the deque.
      */
-    public int size();
+    int size();
 
     /**
      * Prints the items in the duque from first to last, separated by a space.
      */
-    public void printDeque();
+    void printDeque();
 
     /**
      * Removes and returns the item at the front of the deque.
      */
-    public T removeFirst();
+    T removeFirst();
 
     /**
      * Removes and returns the item at the back of the deque.
      */
-    public T removeLast();
+    T removeLast();
 
     /**
      * Gets the item at the given index, where 0 is the front,
      * 1 is the next item, and so forth.
      */
-    public T get(int index);
+    T get(int index);
 
     /**
      * Return an iterator.
      */
-    public Iterator<T> iterator();
+    Iterator<T> iterator();
 }
