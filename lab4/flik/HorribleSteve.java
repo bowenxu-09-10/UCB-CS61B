0238700fbf4edd84e2c49993150d2d032acb2959
Bowen <bowenxu.09.10@gmail.com>
2025-11-15 15:34:30 +0800

    Restructure main() in HS.java

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -2,13 +2,23 @@ package flik;
 
 public class HorribleSteve {
     public static void main(String [] args) throws Exception {
-        int i = 0;
-        for (int j = 0; i < 500; ++i, ++j) {
+        Integer i = 0;
+        Integer j = 0;
+        while (i < 500) {
             if (!Flik.isSameNumber(i, j)) {
                 throw new Exception(
                         String.format("i:%d not same as j:%d ??", i, j));
             }
+            i++;
+            j++;
         }
         System.out.println("i is " + i);
+//        for (int j = 0; i < 500; ++i, ++j) {
+//            if (!Flik.isSameNumber(i, j)) {
+//                throw new Exception(
+//                        String.format("i:%d not same as j:%d ??", i, j));
+//            }
+//        }
+//        System.out.println("i is " + i);
     }
 }
