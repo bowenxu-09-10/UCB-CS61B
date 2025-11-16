d9bc5e4bca7467ff5a3c70166cbdb3c49d9d91cc
Bowen <bowenxu.09.10@gmail.com>
2025-11-16 14:29:32 +0800

    Create dog folder in .capers

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -2,16 +2,17 @@ package capers;
 
 import java.io.File;
 import java.io.Serializable;
+
+import static capers.CapersRepository.CAPERS_FOLDER;
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
- * @author TODO
+ * @author Bowen
 */
 public class Dog { // TODO
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
-                                         //      function in Utils)
+    static final File DOG_FOLDER = Utils.join(CAPERS_FOLDER, "dogs");
 
     /** Age of dog. */
     private int age;
