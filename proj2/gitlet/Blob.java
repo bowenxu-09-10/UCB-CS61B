2fd4efb4176c22ce4e886fdd4cb755d2007b05da
Bowen <bowenxu.09.10@gmail.com>
2025-11-18 20:36:42 +0800

    Create Blob class

---
+++ proj2/gitlet/Blob.java
@@ -0,0 +1,13 @@
+package gitlet;
+
+import java.io.File;
+import static gitlet.Utils.*;
+
+public class Blob {
+    /** The blob folder contains blob object. */
+    public static final File BLOB_FOLDER = join(Repository.GITLET_DIR, "blob");
+
+    Blob() {
+
+    }
+}
