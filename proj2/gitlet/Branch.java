7e8d3d44655916bd25f0dc6375bab43e4a1d050a
Bowen <bowenxu.09.10@gmail.com>
2025-11-18 16:53:49 +0800

    Create func for creating new branch and write commitPID

--- proj2/gitlet/Branch.java
+++ proj2/gitlet/Branch.java
@@ -1,6 +1,7 @@
 package gitlet;
 
 import java.io.File;
+import java.io.IOException;
 
 import static gitlet.Utils.*;
 
@@ -8,6 +9,7 @@ public class Branch {
 
     /** The HEAD reference. */
     public static final File HEAD = join(Repository.GITLET_DIR, "HEAD");
+    public static final File BRANCH_DIR = join(Repository.GITLET_DIR, "branches");
 
     /** Read Head and get the current branch name. */
     public static String readHead() {
@@ -19,4 +21,19 @@ public class Branch {
         writeContents(HEAD, readHead());
     }
 
+    /** Create a new branch file. */
+    public void newBranch(String name) {
+        File branch = join(BRANCH_DIR, name);
+        try {
+            branch.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // ToDo write commitPID into the branch file
+    public void writeBranch(String commitPID, String name) {
+        File branch = join(BRANCH_DIR, name);
+        writeContents(branch, commitPID);
+    }
 }
