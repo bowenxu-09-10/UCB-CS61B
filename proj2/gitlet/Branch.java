6baa9888e544edfa1b05837a094ed75f8351d42f
Bowen <bowenxu.09.10@gmail.com>
2025-11-18 16:44:34 +0800

    Add HEAD file reference

--- proj2/gitlet/Branch.java
+++ proj2/gitlet/Branch.java
@@ -1,17 +1,22 @@
 package gitlet;
 
+import java.io.File;
+
 import static gitlet.Utils.*;
 
 public class Branch {
 
+    /** The HEAD reference. */
+    public static final File HEAD = join(Repository.GITLET_DIR, "HEAD");
+
     /** Read Head and get the current branch name. */
     public static String readHead() {
-        return readContentsAsString(Repository.HEAD);
+        return readContentsAsString(HEAD);
     }
 
     /** Write current branch name into the HEAD. */
     public static void writeHead() {
-        writeContents(Repository.HEAD, readHead());
+        writeContents(HEAD, readHead());
     }
 
 }
