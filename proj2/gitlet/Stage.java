6537b0ef826b2941590f9ac02df3917c0b5aa047
Bowen <bowenxu.09.10@gmail.com>
2025-11-19 16:38:12 +0800

    Create saveFile and checkSame functins
    - saveFile(): save given file as blob.
    - checkSame(): check if given file is the same as commit one.

--- proj2/gitlet/Stage.java
+++ proj2/gitlet/Stage.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Set;
 
-import static gitlet.Utils.join;
+import static gitlet.Utils.*;
 
 public class Stage {
     /** A Map for staged file for addition. */
@@ -14,4 +14,24 @@ public class Stage {
 
     /** The index file acts as staging area. */
     public static final File INDEX = join(Repository.GITLET_DIR, "index");
+
+    /** Save file as blob file, and return whether the file already exist.
+     *  True for exist, false for non-exist. */
+    private static boolean saveFile(String fileName) {
+        // Read contents in file and make a blob.
+        Blob blob = new Blob(fileName);
+        return blob.saveBlob();
+    }
+
+    /** Check if current file added is the same as commited.
+     *  True for is the same as committed. */
+    private static boolean checkSame(String fileName) {
+        String pid = sha1(fileName);
+        Commit head = Commit.getHeadCommit();
+        HashMap fileTracted = head.fileNameToBLOB;
+        if (fileTracted.containsValue(pid)) {
+            return true;
+        }
+        return false;
+    }
 }
