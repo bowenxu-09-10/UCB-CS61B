1b97aa1edb224f0f7a94b6f945f38dc7a94a33b9
Bowen <bowenxu.09.10@gmail.com>
2025-12-05 17:38:43 +0800

    Create basic skeleton

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,71 @@
+package bstmap;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K, V> implements Map61B<K, V> {
+    /** Removes all of the mappings from this map. */
+    @Override
+    public void clear() {
+        // Todo
+        throw new UnsupportedOperationException();
+    }
+
+
+    /** Returns true if this map contains a mapping for the specified key. */
+    @Override
+    public boolean containsKey(K key) {
+        // Todo
+        throw new UnsupportedOperationException();
+    }
+
+    /** Returns the value to which the specified key is mapped, or null if this
+     *  map contains no mapping for the key.
+     */
+    @Override
+    public V get(K key) {
+        // Todo
+        throw new UnsupportedOperationException();
+    }
+
+    /** Returns the number of key-value mappings in this map. */
+    @Override
+    public int size() {
+        // Todo
+        throw new UnsupportedOperationException();
+    }
+
+    /** Associates the specified value with the specified key in this map. */
+    @Override
+    public void put(K key, V value) {
+        // Todo
+        throw new UnsupportedOperationException();
+    }
+
+    /** Prints out BSTMap in order of increasing Key. */
+    public void printInOrder() {
+        // Todo
+        throw new UnsupportedOperationException();
+    }
+
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+}
